5d0f10bfbaa9546d682c2e985c4a645a077f54fd
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-03 23:27:43 +0100

    project setup

---
+++ voting/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # path('', HomeView.as_view(), name='home'),
+    # path('candidates/', CandidatesView.as_view(), name='candidates'),
+    # path('candidates/<int:pk>/', CandidateDetailView.as_view(), name='candidate_detail'),
+    # path('candidates/<int:pk>/vote/', vote, name='vote'),
+    # path('results/', ResultsView.as_view(), name='results'),
+    # path('admin/', admin.site.urls),
+]
