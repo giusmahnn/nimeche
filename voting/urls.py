97495035859f7eec4b63904fd49bdf33eb57bd8d
davidinmichael <davidinmichael@gmail.com>
2025-01-04 11:54:19 +0100

    Added templates.

--- voting/urls.py
+++ voting/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import (
+    HomePage,
+)
 
 
 urlpatterns = [
-    # path('', HomeView.as_view(), name='home'),
+    path('', HomePage.as_view(), name='home'),
     # path('candidates/', CandidatesView.as_view(), name='candidates'),
     # path('candidates/<int:pk>/', CandidateDetailView.as_view(), name='candidate_detail'),
     # path('candidates/<int:pk>/vote/', vote, name='vote'),
