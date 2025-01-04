9a510fe66e1ce01d3985cef38b75123767500a09
davidinmichael <davidinmichael@gmail.com>
2025-01-04 11:15:33 +0100

    Added models in voting app.

--- nimeche/settings/base.py
+++ nimeche/settings/base.py
@@ -77,13 +77,6 @@ WSGI_APPLICATION = 'nimeche.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -119,7 +112,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
--- voting/models.py
+++ voting/models.py
@@ -2,24 +2,33 @@ from django.db import models
 
 # Create your models here.
 
-
-class Candidate(models.Model):
-    name = models.CharField(max_length=100)
-    introduction = models.TextField()
-    image = models.ImageField(upload_to='candidates')
-    votes = models.IntegerField(default=0)
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+class Position(BaseModel):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+
+class Candidate(BaseModel):
+    name = models.CharField(max_length=100)
+    introduction = models.TextField()
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(upload_to='candidates/', blank=True, null=True)
+    votes = models.IntegerField(default=0)
+    
     def __str__(self):
         return f"{self.name} - {self.vote}"
     
 
-class Voter(models.Model):
+class Voter(BaseModel):
     matric_number = models.CharField(max_length=100)
-    ip_address = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    ip_address = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.matric_number}"
