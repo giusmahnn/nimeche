2528e2640b8bcfd68e68e1d39bdc411a5fbebb98
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-05 19:19:12 +0100

    url removed

--- voting/urls.py
+++ voting/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import (
     HomePage,
-    test_template
 )
 
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
-    path('test/', test_template, name='test'),
     # path('candidates/', CandidatesView.as_view(), name='candidates'),
     # path('candidates/<int:pk>/', CandidateDetailView.as_view(), name='candidate_detail'),
     # path('candidates/<int:pk>/vote/', vote, name='vote'),
