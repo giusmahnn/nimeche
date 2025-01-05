5f4604684f666f26ef5f46dddd285948dd85b161
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-05 01:34:59 +0100

    custom command implemented

--- voting/admin.py
+++ voting/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Position, Candidate, Voter
 
 # Register your models here.
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'votes')
+
+
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ('matric_number', 'ip_address')
+
+
+
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Candidate, CandidateAdmin)
+admin.site.register(Voter, VoterAdmin)
---
+++ voting/management/commands/delete_data.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand  
+from voting.models import Position, Candidate, Voter
+
+class Command(BaseCommand):  
+    help = 'Remove all data from Positions, Candidates, and Voters tables'  
+
+    def handle(self, *args, **options):  
+        
+        confirmed = input("Are you sure you want to delete all Positions, Candidates, and Voters? (yes/no): ")  
+        if confirmed.lower() != 'yes':  
+            self.stdout.write(self.style.WARNING('Deletion canceled.'))  
+            return  
+
+        Position.objects.all().delete()  
+        Candidate.objects.all().delete()  
+        Voter.objects.all().delete()  
+
+        self.stdout.write(self.style.SUCCESS('Successfully deleted all data from Positions, Candidates, and Voters.'))
