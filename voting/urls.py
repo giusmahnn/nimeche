98980d3d1ab18c2dabbb6644469262480324b026
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-07 00:42:16 +0100

    candidates implemented

--- voting/urls.py
+++ voting/urls.py
@@ -7,7 +7,7 @@ from .views import (
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
-    path("detail/", DetailPage.as_view(), name='vote-detail'),
+    path("detail/<int:position_id>/", DetailPage.as_view(), name='vote-detail'),
     # path('candidates/', CandidatesView.as_view(), name='candidates'),
     # path('candidates/<int:pk>/', CandidateDetailView.as_view(), name='candidate_detail'),
     # path('candidates/<int:pk>/vote/', vote, name='vote'),
--- voting/views.py
+++ voting/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+
+from voting.models import Candidate, Position
 
 
 class HomePage(View):
 	def get(self, request):
-		return render(request, "voting/home.html")
+		position = Position.objects.all()
+		context = {
+            "positions": position,
+            }
+		return render(request, "voting/home.html", context)
 	
 
 class DetailPage(View):
-	def get(self, request):
-		return render(request, "voting/vote-detail.html")
+	def get(self, request, position_id):
+		position = get_object_or_404(Position, id=position_id)
+		candidates = Candidate.objects.filter(position=position)
+		context = {
+			"position": position, 
+			"candidates": candidates
+			}
+		return render(request, "voting/vote-detail.html", context)
