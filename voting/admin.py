cb7dc0b5a748b09e9f355565ef33726223d6ee2e
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-07 00:47:54 +0100

    admin display

--- voting/admin.py
+++ voting/admin.py
@@ -4,19 +4,28 @@ from .models import Position, Candidate, Voter
 # Register your models here.
 
 
+from django.contrib import admin
+from .models import Position, Candidate, Voter
+
+# Register your models here.
+
+
+@admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
+    search_fields = ('name',)
+    list_filter = ('name',)
 
 
+@admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
     list_display = ('name', 'position', 'votes')
+    search_fields = ('name', 'position__name')
+    list_filter = ('position',)
 
 
+@admin.register(Voter)
 class VoterAdmin(admin.ModelAdmin):
     list_display = ('matric_number', 'ip_address')
-
-
-
-admin.site.register(Position, PositionAdmin)
-admin.site.register(Candidate, CandidateAdmin)
-admin.site.register(Voter, VoterAdmin)
+    search_fields = ('matric_number',)
+    list_filter = ('matric_number',)
