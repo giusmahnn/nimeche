50153a7389484aa5ff0b97d3e22a1436495a2efc
davidinmichael <davidinmichael@gmail.com>
2025-01-06 13:46:03 +0100

    Added the detail page

--- voting/urls.py
+++ voting/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     HomePage,
+	DetailPage,
 )
 
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
+    path("detail/", DetailPage.as_view(), name='vote-detail'),
     # path('candidates/', CandidatesView.as_view(), name='candidates'),
     # path('candidates/<int:pk>/', CandidateDetailView.as_view(), name='candidate_detail'),
     # path('candidates/<int:pk>/vote/', vote, name='vote'),
