fbcff00b7d99a22692a466f2a4a700edcdf06276
Remigius Mgbeme <garethremigius@gmail.com>
2025-01-13 15:07:06 +0100

    session security

--- voting/admin.py
+++ voting/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from .models import Position, Candidate, Voter
-
-# Register your models here.
-
-
 from django.contrib import admin
 from .models import Position, Candidate, Voter
-
+from django.contrib import admin
 # Register your models here.
 
 
